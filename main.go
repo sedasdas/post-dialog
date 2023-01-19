cfebfa152e234dc994cef5f8bd7012f3b76e3eab
sedasdas <qq1820029262@gmail.com>
2023-01-19 11:34:09 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	jsonrpc "github.com/filecoin-project/go-jsonrpc"
+	lotusapi "github.com/filecoin-project/lotus/api"
+)
+
+func main() {
+	authToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.4tDmJiysQVzdMgpu70bvQHh1poD3pAv30MQsdW770fQ"
+	headers := http.Header{"Authorization": []string{"Bearer " + authToken}}
+	addr := "127.0.0.1:1234"
+
+	var api lotusapi.FullNodeStruct
+	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+addr+"/rpc/v0", "Filecoin", []interface{}{&api.Internal, &api.CommonStruct.Internal}, headers)
+	if err != nil {
+		log.Fatalf("connecting with lotus failed: %s", err)
+	}
+	defer closer()
+
+	// Now you can call any API you're interested in.
+	tipset, err := api.ChainHead(context.Background())
+	if err != nil {
+		log.Fatalf("calling chain head: %s", err)
+	}
+	fmt.Printf("Current chain head is: %s", tipset.String())
+}
