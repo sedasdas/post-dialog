c583bea933a566fe4ee075066e2c230d20c2972a
sedasdas <qq1820029262@gmail.com>
2023-01-20 10:15:57 +0800

    1

--- tools/wallet.go
+++ tools/wallet.go
@@ -5,6 +5,7 @@ import (
 	"github.com/filecoin-project/go-address"
 	lotusapi "github.com/filecoin-project/lotus/api"
 	"log"
+	"strconv"
 )
 
 func GetWalletBalance(ctx context.Context, filename string, api lotusapi.FullNodeStruct) {
@@ -12,7 +13,10 @@ func GetWalletBalance(ctx context.Context, filename string, api lotusapi.FullNod
 	for _, k := range walletlist {
 		add, _ := address.NewFromString(k)
 		balance, _ := api.WalletBalance(ctx, add)
-		log.Print(balance.String())
+		n, _ := strconv.ParseInt(balance.String(), 10, 64)
+		// 将数字转换成字符串，并保留小数点后一位
+		result := strconv.FormatFloat(float64(n)/1e18, 'f', 1, 64)
+		log.Print(result)
 	}
 
 }
