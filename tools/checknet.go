ed4001f700cde9f39300fc8c26bdb13265930142
sedasdas <qq1820029262@gmail.com>
2023-03-09 23:26:18 +0800

    del net check

--- tools/checknet.go
+++ tools/checknet.go
@@ -1,14 +1,5 @@
 package tools
 
-import (
-	"log"
-	"net"
-)
-
 func CheckNet() {
-	_, err := net.Dial("tcp", "43.228.180.125:3453")
-	if err != nil {
-		log.Println("网络中断")
-		SendEm("网络中断", []byte("网络中断"))
-	}
+
 }
