cf647e7169013be3237dbf6345ee5784a7eb0a5f
sedasdas <qq1820029262@gmail.com>
2023-12-28 17:48:41 +0800

    添加节点连接检查，发送邮件告警

--- main.go
+++ main.go
@@ -27,6 +27,8 @@ func connectLotusAPI(addr, authToken string) (lotusapi.FullNodeStruct, jsonrpc.C
 		}
 		log.Printf("连接失败: %s, retrying in 5 seconds...", err)
 		time.Sleep(5 * time.Second)
+		tools.SendEm("连接失败", []byte(time.Now().String()))
+
 	}
 
 	return api, closer, err
@@ -40,11 +42,25 @@ func main() {
 	api, closer, _ := connectLotusAPI(addr, authToken)
 	defer closer()
 
+	// 用于控制发送邮件的时间间隔
+	mailTicker := time.NewTicker(10 * time.Minute)
+	defer mailTicker.Stop()
+
+	// 初始设定为 true，表示刚启动程序时可以发送邮件
+	canSendMail := true
+
 	for {
 		tipset, err := api.ChainHead(context.Background())
 		if err != nil {
 			log.Printf("发生故障: %s", err)
 			closer()
+
+			// 如果可以发送邮件（即满足条件）
+			if canSendMail {
+				tools.SendEm("连接失败", []byte(time.Now().String()))
+				canSendMail = false // 设置为 false，表示不可以发送邮件
+			}
+
 			api, closer, err = connectLotusAPI(addr, authToken)
 			continue
 		}
@@ -53,6 +69,14 @@ func main() {
 		tools.CheckPower(context.Background(), home+"/miner-list", api, tipset.Key())
 		tools.GetWalletBalance(context.Background(), home+"/wallet-list", api)
 		tools.CheckNet()
+
+		select {
+		case <-mailTicker.C:
+			// 到达间隔时间后，设置可以发送邮件的条件为 true，以便下一次故障时可以再次发送邮件
+			canSendMail = true
+		default:
+			// 如果还没到间隔时间，继续循环
+		}
 
 		time.Sleep(30 * time.Second)
 	}
