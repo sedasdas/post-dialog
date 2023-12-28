42e8989742d4040cc2c195cdf0c25c02283f8d19
sedasdas <qq1820029262@gmail.com>
2023-12-28 14:23:11 +0800

    优化连接，加入中文提示，合并连接函数

--- main.go
+++ main.go
@@ -12,54 +12,48 @@ import (
 	lotusapi "github.com/filecoin-project/lotus/api"
 )
 
-func main() {
-	home := os.Getenv("HOME")
-	authToken := os.Getenv("TOKEN")
-	headers := http.Header{"Authorization": []string{"Bearer " + authToken}}
-	addr := os.Getenv("ADDR")
-
-	//var minerapi lotusapi.StorageMiner
-
+func connectLotusAPI(addr, authToken string) (lotusapi.FullNodeStruct, jsonrpc.ClientCloser, error) {
 	var api lotusapi.FullNodeStruct
+	headers := http.Header{"Authorization": []string{"Bearer " + authToken}}
 
-	// 连接 Lotus API
 	var closer jsonrpc.ClientCloser
 	var err error
+
 	for {
 		closer, err = jsonrpc.NewMergeClient(context.Background(), "ws://"+addr+"/rpc/v0", "Filecoin", []interface{}{&api.Internal, &api.CommonStruct.Internal}, headers)
 		if err == nil {
-			log.Printf("connected to lotus successfully")
+			log.Printf("连接成功")
 			break
 		}
-		log.Printf("connecting with lotus failed: %s, retrying in 5 seconds...", err)
+		log.Printf("连接失败: %s, retrying in 5 seconds...", err)
 		time.Sleep(5 * time.Second)
 	}
 
+	return api, closer, err
+}
+
+func main() {
+	home := os.Getenv("HOME")
+	authToken := os.Getenv("TOKEN")
+	addr := os.Getenv("ADDR")
+
+	api, closer, _ := connectLotusAPI(addr, authToken)
 	defer closer()
 
-	// 使用 Lotus API
 	for {
 		tipset, err := api.ChainHead(context.Background())
 		if err != nil {
-			log.Printf("calling chain head: %s", err)
+			log.Printf("发生故障: %s", err)
 			closer()
-			for {
-				closer, err = jsonrpc.NewMergeClient(context.Background(), "ws://"+addr+"/rpc/v0", "Filecoin", []interface{}{&api.Internal, &api.CommonStruct.Internal}, headers)
-				if err == nil {
-					log.Printf("reconnected to lotus successfully")
-
-					break
-				}
-				log.Printf("reconnecting with lotus failed: %s, retrying in 5 seconds...", err)
-				time.Sleep(5 * time.Second)
-			}
+			api, closer, err = connectLotusAPI(addr, authToken)
 			continue
 		}
+
 		log.Printf("chain head: %d", tipset.Height())
 		tools.CheckPower(context.Background(), home+"/miner-list", api, tipset.Key())
 		tools.GetWalletBalance(context.Background(), home+"/wallet-list", api)
 		tools.CheckNet()
-		// 在这里使用 Lotus API
+
 		time.Sleep(30 * time.Second)
 	}
 }
