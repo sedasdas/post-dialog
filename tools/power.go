8543055870ccc07e672bf6e47037d27346468fec
sedasdas <qq1820029262@gmail.com>
2024-07-30 15:42:51 +0800

    重构了代码，改为并发检测扇区

--- tools/power.go
+++ tools/power.go
@@ -7,6 +7,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"log"
 	"strconv"
+	"sync"
 )
 
 type Miner struct {
@@ -17,37 +18,63 @@ type Miner struct {
 
 var miners []*Miner
 
-func initMiners(filename string) {
+func initMiners(filename string) error {
 	minerlist := ReadFromConfig(filename)
 	miners = make([]*Miner, len(minerlist))
 	for i, k := range minerlist {
-		maddr, _ := address.NewFromString(string(k))
+		maddr, err := address.NewFromString(string(k))
+		if err != nil {
+			return err
+		}
 		miners[i] = &Miner{Address: maddr}
 	}
+	return nil
 }
 
-func CheckPower(ctx context.Context, filename string, api lotusapi.FullNodeStruct, tipset types.TipSetKey) {
-	if miners == nil {
-		initMiners(filename)
+func checkMinerPower(ctx context.Context, miner *Miner, api lotusapi.FullNodeStruct, tipset types.TipSetKey) error {
+	faults, err := api.StateMinerFaults(ctx, miner.Address, tipset)
+	if err != nil {
+		return err
 	}
+	count, err := faults.Count()
+	if err != nil {
+		return err
+	}
+	log.Printf("%s 错误扇区数量为：%d", miner.Address.String(), count)
 
-	for _, miner := range miners {
-		faults, _ := api.StateMinerFaults(context.Background(), miner.Address, tipset)
-		count, _ := faults.Count()
-		log.Print(miner.Address.String(), "错误扇区数量为：", count)
+	miner.FaultCount = count
 
-		miner.FaultCount = count
-
-		if miner.FaultCount != miner.LastAlertCount {
-			if miner.FaultCount > 10 {
-				if miner.FaultCount > miner.LastAlertCount {
-					SendEm(miner.Address.String(), []byte(miner.Address.String()+"掉算力了，错误扇区数量为："+strconv.FormatUint(count, 10)))
-				}
-				if miner.FaultCount < miner.LastAlertCount {
-					SendEm(miner.Address.String(), []byte(miner.Address.String()+"恢复中，错误扇区数量为："+strconv.FormatUint(count, 10)))
-				}
+	if miner.FaultCount != miner.LastAlertCount {
+		if miner.FaultCount > 10 {
+			if miner.FaultCount > miner.LastAlertCount {
+				SendEm(miner.Address.String(), []byte(miner.Address.String()+"掉算力了，错误扇区数量为："+strconv.FormatUint(count, 10)))
 			}
-			miner.LastAlertCount = miner.FaultCount
+			if miner.FaultCount < miner.LastAlertCount {
+				SendEm(miner.Address.String(), []byte(miner.Address.String()+"恢复中，错误扇区数量为："+strconv.FormatUint(count, 10)))
+			}
+		}
+		miner.LastAlertCount = miner.FaultCount
+	}
+	return nil
+}
+
+func CheckPower(ctx context.Context, filename string, api lotusapi.FullNodeStruct, tipset types.TipSetKey) error {
+	if miners == nil {
+		if err := initMiners(filename); err != nil {
+			return err
 		}
 	}
+
+	var wg sync.WaitGroup
+	for _, miner := range miners {
+		wg.Add(1)
+		go func(m *Miner) {
+			defer wg.Done()
+			if err := checkMinerPower(ctx, m, api, tipset); err != nil {
+				log.Printf("检查矿工 %s 时出错: %v", m.Address.String(), err)
+			}
+		}(miner)
+	}
+	wg.Wait()
+	return nil
 }
