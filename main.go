53e58e3f855d5cad7147a3a4701c364aef5fbd53
sedasdas <qq1820029262@gmail.com>
2024-09-27 11:10:05 +0800

    恢复

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -14,7 +15,7 @@ import (
 
 var sTime = time.Now()
 
-func connectLotusAPI(ctx context.Context, addr, authToken string) (lotusapi.FullNodeStruct, jsonrpc.ClientCloser, error) {
+func connectLotusAPI(addr, authToken string) (lotusapi.FullNodeStruct, jsonrpc.ClientCloser, error) {
 	var api lotusapi.FullNodeStruct
 	headers := http.Header{"Authorization": []string{"Bearer " + authToken}}
 
@@ -22,25 +23,28 @@ func connectLotusAPI(ctx context.Context, addr, authToken string) (lotusapi.Full
 	var err error
 
 	for {
-		select {
-		case <-ctx.Done():
-			return api, nil, ctx.Err()
-		default:
-			closer, err = jsonrpc.NewMergeClient(ctx, "ws://"+addr+"/rpc/v0", "Filecoin", []interface{}{&api.Internal, &api.CommonStruct.Internal}, headers)
-			if err == nil {
-				log.Printf("连接成功")
-				return api, closer, nil
-			}
-			log.Printf("连接失败: %s, 5秒后重试...", err)
-			time.Sleep(5 * time.Second)
-			elapsedTime := time.Since(sTime)
-			if elapsedTime > 10*time.Minute {
-				log.Println("已经过了十分钟")
-				tools.SendEm("连接失败", []byte(time.Now().String()))
-				sTime = time.Now()
-			}
+		closer, err = jsonrpc.NewMergeClient(context.Background(), "ws://"+addr+"/rpc/v0", "Filecoin", []interface{}{&api.Internal, &api.CommonStruct.Internal}, headers)
+		if err == nil {
+			log.Printf("连接成功")
+			break
 		}
+		log.Printf("连接失败: %s, retrying in 5 seconds...", err)
+		time.Sleep(5 * time.Second)
+		// 检查是否已经过了十分钟
+		elapsedTime := time.Since(sTime)
+		if elapsedTime > 10*time.Minute {
+			// 已经过了十分钟
+			fmt.Println("已经过了十分钟")
+			tools.SendEm("连接失败", []byte(time.Now().String()))
+			sTime = time.Now()
+		} else {
+			// 还未过十分钟
+			fmt.Println("还未过十分钟")
+		}
+
 	}
+
+	return api, closer, err
 }
 
 func main() {
@@ -48,51 +52,22 @@ func main() {
 	authToken := os.Getenv("TOKEN")
 	addr := os.Getenv("ADDR")
 
+	api, closer, _ := connectLotusAPI(addr, authToken)
+	defer closer()
+
 	for {
-		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
-		api, closer, err := connectLotusAPI(ctx, addr, authToken)
+		tipset, err := api.ChainHead(context.Background())
 		if err != nil {
-			log.Printf("连接 Lotus API 失败: %v", err)
-			cancel()
-			time.Sleep(30 * time.Second)
+			log.Printf("发生故障: %s", err)
+			closer()
+			api, closer, err = connectLotusAPI(addr, authToken)
 			continue
 		}
-
-		log.Printf("Home 目录: %s", home)
-
-		for {
-			select {
-			case <-ctx.Done():
-				log.Println("上下文已取消，重新连接")
-				closer()
-				break
-			default:
-				tipset, err := api.ChainHead(ctx)
-				if err != nil {
-					log.Printf("获取链头失败: %v", err)
-					time.Sleep(30 * time.Second)
-					continue
-				}
-
-				log.Printf("链高度: %d", tipset.Height())
-
-				if err := tools.CheckPower(ctx, home+"/miner-list", api, tipset.Key()); err != nil {
-					log.Printf("检查算力失败: %v", err)
-				}
-
-				// 如果需要，可以取消注释以下行
-				// if err := tools.GetWalletBalance(ctx, home+"/wallet-list", api); err != nil {
-				//     log.Printf("获取钱包余额失败: %v", err)
-				// }
-				// if err := tools.CheckNet(); err != nil {
-				//     log.Printf("检查网络失败: %v", err)
-				// }
-
-				time.Sleep(30 * time.Second)
-			}
-		}
-
-		closer()
-		cancel()
+		log.Printf(home)
+		log.Printf("chain head: %d", tipset.Height())
+		tools.CheckPower(context.Background(), home+"/miner-list", api, tipset.Key())
+		//tools.GetWalletBalance(context.Background(), home+"/wallet-list", api)
+		//tools.CheckNet()
+		time.Sleep(30 * time.Second)
 	}
 }
